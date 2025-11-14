99b05d1bf54e852c484b60ad81f3ec6dd1394a6b
Alejandro Benavides <alejandro@meddyg.com>
2025-11-14 11:31:05 -0600

    fix: eliminar configuración de git y script de reconstrucción de sushi

--- re-build-sushi.sh
+++
@@ -1,6 +0,0 @@
-docker rm sushi
-docker rmi hl7cr/sushi:v1
-cd /home/ale/Proyectos/HL7\ CR/terminology.git/
-docker build -t hl7cr/sushi:v1 .
-cd /home/ale/Proyectos/HL7\ CR/
-docker run -it --name sushi -v "$(pwd)/terminology.git:/ig" hl7cr/sushi:v1
